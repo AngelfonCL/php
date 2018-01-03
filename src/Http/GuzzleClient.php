d25f3f212727c9608916628ff37d156deeb34846
Fernando Mora <fernando@angelfon.com>
2018-01-03 10:39:24 -0300

    configura guzzle 6 para su uso como cliente HTTP

--- src/Http/GuzzleClient.php
+++ src/Http/GuzzleClient.php
@@ -9,9 +9,23 @@ class GuzzleClient implements Client {
 	public $lastRequest = null;
 	public $lastResponse = null;
 
-	public function request($method, $url, $params = [], $data = [], $headers = [], $timeout = null)
+	public function request($method, $url, $params = [], $data = [], $headers = [], $timeout = DEFAULT_TIMEOUT)
 	{
+		$client = new HttpClient();
 
+		$request = $client->request($method, $url, array(
+			'headers' => $headers,
+			'json' => $body,
+			'query' => $params,
+			'timeout' => $timeout,
+			'http_errors' => false
+		));
+
+		$response = $request->send();
+
+		$statusCode = $response->getStatusCode();
+		$body = $response->getBody();
+		$responseHeaders = $response->getHeaders();
 
 		$this->lastResponse = new Response($statusCode, $body, $responseHeaders);
 		return $this->lastResponse;
